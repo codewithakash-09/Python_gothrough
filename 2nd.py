352039804b858be46e77d980bef59a5d3110098c
Akash Gautam <codewithakash09@gmail.com>
2026-04-13 10:02:36 +0530

    first commit

---
+++ 2nd.py
@@ -0,0 +1,12 @@
+# a= 35
+# B="happy "+ str(a)+ "th birthday"
+# print(B)
+# print(3+5)
+# print(13-5)
+# print(2*4)
+# print(16/2)
+# a=int(input("Enter your favourtie  number: "))
+# print("Your favourtie number is: ",a)
+list = ['suzan','sara','sarah','sana']
+print(list[0])
+print(list[2].title())
---
+++ 3rd.py
@@ -0,0 +1,14 @@
+# li=['trek','cannondale','giant','specialized']
+# print(li[0].title())
+# print(li[1].title())
+# print(li[2].title())
+# print(li[3].title())
+# li=['trek','cannondale','giant','specialized']
+# a="most popular bike brands are "+li[0].title()+"."
+# print(a)
+l=[]
+l.append('trek')
+l.append('cannondale')    
+l.append('giant')
+l.append('specialized')
+print(l[0])
