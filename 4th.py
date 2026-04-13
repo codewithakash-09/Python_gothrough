4fb231fcdd614a484a02100bbc1f2325a4afb913
Akash Gautam <codewithakash09@gmail.com>
2026-04-13 10:36:29 +0530

    first commit

---
+++ 4th.py
@@ -0,0 +1,8 @@
+# printing numvbers from 0 to 99
+# for i in range(1, 100):
+# #     print(i)
+# #print even numbers frpom 1 to n
+# n=int(input("enter the number: "))
+# for i in range(1, n+1):
+#     if i%2==0:
+#         print(i)
